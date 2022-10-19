cdb85e936faa30631e7f2e01aead30083001c4e5
Bruno <brunobrito728@gmail.com>
2022-10-19 16:42:40 -0300

    finalizado o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Teste o componente notFound.js', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not foundTeste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const titleNotFound = screen.getByRole('heading', { level: 2 });
+    expect(titleNotFound).toHaveTextContent('Page requested not found');
+  });
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img');
+    expect((image).src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
